df0f3d4733a54bf33d0b535fdd032f0784f3ecf6
succubus-q <rrkk200410@gmail.com>
2024-09-27 19:35:24 +0000

    added only code from lab2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,13 @@
 #include "revert_string.h"
+#include <string.h>
 
 void RevertString(char *str)
 {
-	// your code here
+    int length = strlen(str);
+    for (int i = 0; i < length / 2; i++) {
+        // Меняем местами символы на позициях i и (length - i - 1)
+        char temp = str[i];
+        str[i] = str[length - i - 1];
+        str[length - i - 1] = temp;
+    }
 }
-
